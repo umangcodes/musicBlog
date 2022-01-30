1d2398905999d586ba39ab75f4044725aacb7863
Umang A <umanga.eng@gmail.com>
2022-01-30 14:35:57 -0500

    changed the title of the page

--- src/components/navigation/Header.js
+++ src/components/navigation/Header.js
@@ -6,8 +6,10 @@ function Header(props) {
     </div>
   ));
   return (
-    <div className="sticky top-5 z-10 bg-opacity-100 grid lg:grid-cols-3 grid-cols-2 my-5 mx-5">
-      <div className="col-start-1 flex lg:px-10 px-5 text-2xl">IMG</div>
+    <div className="sticky top-5 z-10 bg-opacity-80 grid lg:grid-cols-3 grid-cols-2 my-5 mx-5">
+      <div className="col-start-1 flex lg:px-10 px-5 lg:text-2xl text-xl">
+        SM
+      </div>
       <div className="lg:col-start-3 cols-start-2 flex justify-end items-center">
         {list}
       </div>
