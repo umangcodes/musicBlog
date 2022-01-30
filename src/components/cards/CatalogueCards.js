db7d631a0bd28dfc037f9ec2795c373aaba5444e
Umang A <umanga.eng@gmail.com>
2022-01-30 14:35:15 -0500

    basic card outline created

--- src/components/cards/CatalogueCards.js
+++ src/components/cards/CatalogueCards.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React from "react";
 
 export default function CatalogueCards() {
-  return <div></div>;
+  return <div className="h-96 w-96 border-2 rounded-xl bg-white"></div>;
 }
+
+
