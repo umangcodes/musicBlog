dd90c41f207cf96d990058ed3a5070ce43c731ce
Umang A <umanga.eng@gmail.com>
2022-01-30 19:54:20 -0500

    intro created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Home() {
-  return <div className="h-screen bg-red-100"></div>;
+  return (
+    <div className="flex lg:h-96 justify-center items-center border-2 py-5">
+      <p className="">Intro</p>
+    </div>
+  );
 }
