183740ffe5012ef0b3702775c6c7871bbe5d5c2b
Umang A <umanga.eng@gmail.com>
2022-01-30 19:55:26 -0500

    added catalogue cards to the services page. cards centered.

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,11 +1,39 @@
 import CatalogueCards from "../components/cards/CatalogueCards";
-
 import React from "react";
 
 export default function Services() {
+  const typesOfService = [
+    {
+      cardId: 1,
+      cardTitle: "string",
+      cardTitleImg: "url",
+      cardDesc: "string",
+      subscriptionType: "string",
+    },
+    {
+      cardId: 2,
+      cardTitle: "string",
+      cardTitleImg: "url",
+      cardDesc: "string",
+      subscriptionType: "string",
+    },
+    {
+      cardId: 3,
+      cardTitle: "string",
+      cardTitleImg: "url",
+      cardDesc: "string",
+      subscriptionType: "string",
+    },
+  ];
+  let listOfServices = typesOfService.map((service) => (
+    <CatalogueCards key={service.cardId} data={service} />
+  ));
   return (
-    <div>
-      <CatalogueCards />
+    <div className="py-5 px-5">
+      <p className="text-center text-2xl pb-5">Services</p>
+      <div className="flex flex-col md:flex-row justify-center">
+        {listOfServices}
+      </div>
     </div>
   );
 }
